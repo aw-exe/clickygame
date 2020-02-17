69ba232fb634f4ff239584a59e53f271944005d1
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2020-02-16 19:54:00 -0600

    Added a jumbtron inside of my index.js file inside of the jumbotron folder.

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+const jumbotron = () => {
+
+    return(
+    
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Rick and Morty Clicky Game!</h1>
+                <p class="lead">Click on an image to earn points, but don't click an image more than once!</p>
+            </div>
+        </div>
+    );
+};
+    
+export default jumbotron;
