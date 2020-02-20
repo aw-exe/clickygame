897d96368f1277530dd2f15fc9a5af7d17f27549
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2020-02-19 20:58:53 -0600

    Added Bootstrap CDN link to the index.html inside of the public folder so my boostrap styling will be visible when application is deployed. Bootrap styling is visible in localhost.

--- src/components/Layout/Wrapper/index.js
+++ src/components/Layout/Wrapper/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function Wrapper(props) {
-  return <div className="wrapper">{props.children}</div>;
+function Wrapper (props) {
+  return(
+    <div className="container mb-5 mt-5 pt-5">
+      <div className="row">
+        {props.children}
+      </div>
+    </div>
+  )
 }
 
 export default Wrapper;
